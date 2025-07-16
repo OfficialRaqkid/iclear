0be592ed42cece79c7358f2155003b27bb83bfdb
unknown <you@example.com>
2025-07-16 17:28:16 +0800

    All navbar

--- resources/views/dashboard/student/partials/navbar.blade.php
+++ resources/views/dashboard/student/partials/navbar.blade.php
@@ -28,6 +28,9 @@
             <div class="dropdown az-header-notification">
                 <a href="#" class="new"><i class="typcn typcn-bell"></i></a>
                 <div class="dropdown-menu">
+                    <div class="az-dropdown-header mg-b-20 d-sm-none">
+                        <a href="" class="az-header-arrow"><i class="icon ion-md-arrow-back"></i></a>
+                    </div>
                     <h6 class="az-notification-title">Notifications</h6>
                     <p class="az-notification-text">You have 2 new notifications</p>
                     <div class="az-notification-list">
